30951da1f665c941271663970c862fc1d7b1a81d
vinitha <vinitha.k@marketsimplified.com>
2022-04-27 17:05:57 +0530

    sub

--- sumofthenumber.java
+++ sumofthenumber.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class sumofthenumber {
     public static void main(String[] args) {
-        int sum, x, y;
+        int sum, sub, x, y;
         Scanner sc = new Scanner(System.in);
         System.out.println("enter the numbers");
         x = sc.nextInt();
         y = sc.nextInt();
         sum = x + y;
+        sub = x - y;
         System.out.println("sum of the number" + sum);
 
     }
